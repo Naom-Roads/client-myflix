e21ac8ca35ac049289a7258f4c897a53706a273b
Naom-Roads <nrrodrig@gmail.com>
2021-10-28 18:28:32 +0200

    Configured Movie Card Proptypes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,6 +14,7 @@ export function LoginView(props) {
     };
 
     return (
+        <Container classname="login-card">
         <form>
             <label>
                 Username:
@@ -25,5 +26,6 @@ export function LoginView(props) {
             </label>
             <button type="submit" onClick={handleSubmit}>Submit</button>
         </form>
+        </Container>
     );
 }
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -15,8 +15,15 @@ MovieCard.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
-        director: PropTypes.object,
-        genres: PropTypes.array,
+        director: PropTypes.shape({
+            name: PropTypes.string,
+            bio: PropTypes.string,
+            birthdate: PropTypes.instanceOf(Date),
+        }),
+        genres: PropTypes.shape({
+            name: PropTypes.string,
+            description: PropTypes.string
+        }),
         ImagePath: PropTypes.string
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired
