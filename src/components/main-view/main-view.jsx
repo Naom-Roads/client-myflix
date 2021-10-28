b99e63f0780eadf42a09b83da5a527bc6f7fb707
Naom-Roads <nrrodrig@gmail.com>
2021-10-28 16:34:45 +0200

    Deploying app with main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 export class MainView extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                {_id: 1, title: 'Inception', description: 'desc1...', ImagePath: '...'}
+            ]
+        }
+    }
+
     render() {
+        const {movies} = this.state;
+
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
         return (
-            <>
-                <div className="main-view">
-                    <div>The Shawshank Redemption</div>
-                </div>
-                <button>Test</button>
-            </>
+            <div className="main-view">
+                {movies.map(movie => <div key={movie._id}>{movie.title}</div>)}
+            </div>
         );
     }
 }
 
-export default MainView;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MainView from './components/main-view/main-view';
+import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
