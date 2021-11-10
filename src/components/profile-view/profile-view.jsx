c8e05520d201dd5b8ef094a06fedbf95e8b901c5
Naom-Roads <nrrodrig@gmail.com>
2021-11-10 18:53:27 +0100

    Added fields for form

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {isDate} from "../../../dist/index.6701a6e1";
-import {Container} from "react-bootstrap";
+import {Col, Container, FormGroup} from "react-bootstrap";
 import {FavoriteMovies} from "./favorite-movies";
 import axios from "axios";
+import {UpdateUser} from "./update-user";
+import {Redirect, Route} from "react-router-dom";
+import {RegistrationView} from "../registration-view/registration-view";
 
 
 export class ProfileView extends React.Component {
@@ -31,8 +34,21 @@ export class ProfileView extends React.Component {
             user: null
         });
 
-        // Gets User
     }
+
+    onUpdateUser(user) {
+        this.setState({
+            user: authData.user.username
+        });
+
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.username);
+        this.getMovies(authData.token);
+        this.getGenres(authData.token);
+        this.getDirectors(authData.token);
+    }
+
+    // Gets User
 
     getUser() {
         const username = localStorage.getItem('user')
@@ -71,99 +87,43 @@ export class ProfileView extends React.Component {
             })
     }
 
-    // Update User Info
-
-    handleUpdate(e, username, password, email, birthday) {
-        this.state({
-            validated: null,
+    onUpdateUser(user) {
+        this.setState( {
+            user: user,
         });
-
-        const form = e.currentTarget;
-        if (form.checkValidity() === false) {
-            e.preventDefault();
-            e.stopPropagation();
-            this.setState({
-                validated: true,
-            });
-            return;
-        }
-        e.preventDefault();
-
-        const token = localStorage.getItem('token');
-        axios.put(`http://localhost:8000/users/${username}`,
-            {
-                username: this.state.username,
-                password: this.state.password,
-                email: this.state.password,
-                birthday: this.state.birthday
-            },
-            {
-                headers: {Authorization: `Bearer ${token}`}
-            })
-            .then((response) => {
-                alert("Your Profile Has Been Updated");
-                this.setState({
-                    username: this.state.username,
-                    password: this.state.password,
-                    email: this.state.password,
-                    birthday: this.state.birthday
-                });
-                localStorage.setItem('user', this.state.username);
-            })
-            .catch(function (err) {
-                console.log(err);
-            })
-    }
-
-    setUsername(value) {
-        this.state.username
-    }
-
-    setPassword(value) {
-        this.state.password
-    }
-
-    setEmail(value) {
-        this.state.email
-    }
-
-    setBirthday(value) {
-        this.state.birthday = value;
-    }
-
-    // Deletes user
-
-    handleDeleteUser() {
-        const token = localStorage.getItem('token');
-        const username = localStorage.getItem('user');
-        axios.delete(`http://localhost:8000/users/${username}`, {
-            headers: {Authorization `Bearer ${token}`}
-        })
-            .then((response) => {
-                console.log(response);
-                alert(username + 'has been deleted');
-                localStorage.removeItem('user');
-                localStorage.removeItem('token');
-                alert("Your account has been deleted");
-                window.open('/', "_self");
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
     }
 
 
     render() {
 
-        const {user, movies} = this.props;
+        const {FavoriteMovies, username, validated, email, birthday} = this.props;
+        const {movies} = this.props;
+
         return (
 
             <Container>
 
                 <Row>
                     <Col>
+                        <Card>
+
+                            <Card.Title>Profile</Card.Title>
+                            <Card.Body>
+
+                                //Profile Information Goes here
+
+                            </Card.Body>
+                        </Card>
 
                     </Col>
+
+                    <Route path="/users/:username" render={() => {
+                        if (user) return <Redirect to="/"/>
+                        return <Col>
+                            <UpdateUser onUpdateUser={user => this.onUpdateUser(user)}/>
+                        </Col>
+                    }}/>
+
                 </Row>
 
             </Container>
